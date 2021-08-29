0133d784472acdede88dbc58dbffadc04583b11b
vaishnavi-ingole <78408813+vaishnavi-ingole@users.noreply.github.com>
2021-08-29 16:58:39 +0530

    Add files via upload
    Expandable List View

---
+++ ChildInfo.java
@@ -0,0 +1,25 @@
+package com.example.practiceapp1;
+
+import java.util.ArrayList;
+
+public class ChildInfo {
+    private String sequence = "";
+    private String name = "";
+
+//    public String getSequence() {
+//        return sequence;
+//    }
+
+    public void setSequence(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
--- MainActivity.java
+++ MainActivity.java
@@ -1,57 +1,178 @@
-package com.example.testproject;
+package com.example.practiceapp1;
 
 import android.os.Bundle;
-import android.transition.AutoTransition;
-import android.transition.TransitionManager;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
-    ImageButton arrow;
-    LinearLayout hiddenView;
-    CardView cardView;
+public class MainActivity extends AppCompatActivity{
+
+    private LinkedHashMap<String, GroupInfo> subjects = new LinkedHashMap<String, GroupInfo>();
+    private ArrayList<GroupInfo> deptList = new ArrayList<GroupInfo>();
+
+    private CustomAdapter listAdapter;
+    private ExpandableListView simpleExpandableListView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cardView = findViewById(R.id.base_cardview);
-        arrow = findViewById(R.id.arrow_button);
-        hiddenView = findViewById(R.id.hidden_view);
+        // add data for displaying in expandable list view
+        loadData();
 
-        arrow.setOnClickListener(new View.OnClickListener() {
+        //get reference of the ExpandableListView
+        simpleExpandableListView = (ExpandableListView) findViewById(R.id.simpleExpandableListView);
+        // create the adapter by passing your ArrayList data
+        listAdapter = new CustomAdapter(MainActivity.this, deptList);
+        // attach the adapter to the expandable list view
+        simpleExpandableListView.setAdapter(listAdapter);
+
+        //expand all the Groups
+        expandAll();
+
+        // setOnChildClickListener listener for child row click
+        simpleExpandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
             @Override
-            public void onClick(View view) {
-
-                // If the CardView is already expanded, set its visibility
-                //  to gone and change the expand less icon to expand more.
-                if (hiddenView.getVisibility() == View.VISIBLE) {
-
-                    // The transition of the hiddenView is carried out
-                    //  by the TransitionManager class.
-                    // Here we use an object of the AutoTransition
-                    // Class to create a default transition.
-                    TransitionManager.beginDelayedTransition(cardView,
-                            new AutoTransition());
-                    hiddenView.setVisibility(View.GONE);
-                    arrow.setImageResource(R.drawable.ic_baseline_expand_more_24);
-                }
-
-                // If the CardView is not expanded, set its visibility
-                // to visible and change the expand more icon to expand less.
-                else {
-
-                    TransitionManager.beginDelayedTransition(cardView,
-                            new AutoTransition());
-                    hiddenView.setVisibility(View.VISIBLE);
-                    arrow.setImageResource(R.drawable.ic_baseline_expand_less_24);
-                }
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                //get the group header
+                GroupInfo headerInfo = deptList.get(groupPosition);
+                //get the child info
+                ChildInfo detailInfo =  headerInfo.getProductList().get(childPosition);
+                //display it or do something with it
+                Toast.makeText(getBaseContext(), " Clicked on :: " + headerInfo.getName()
+                        + "/" + detailInfo.getName(), Toast.LENGTH_LONG).show();
+                return false;
             }
         });
+        // setOnGroupClickListener listener for group heading click
+        simpleExpandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                //get the group header
+                GroupInfo headerInfo = deptList.get(groupPosition);
+                //display it or do something with it
+                Toast.makeText(getBaseContext(), " Header is :: " + headerInfo.getName(),
+                        Toast.LENGTH_LONG).show();
+
+                return false;
+            }
+        });
+
+
     }
+
+    //method to expand all groups
+    private void expandAll() {
+        int count = listAdapter.getGroupCount();
+        for (int i = 0; i < count; i++){
+            simpleExpandableListView.expandGroup(i);
+        }
+    }
+
+    //method to collapse all groups
+    private void collapseAll() {
+        int count = listAdapter.getGroupCount();
+        for (int i = 0; i < count; i++){
+            simpleExpandableListView.collapseGroup(i);
+        }
+    }
+
+    //load some initial data into out list
+    private void loadData(){
+
+        addProduct("Company1","Details1");
+        addProduct("Company1","Details2");
+        addProduct("Company1","Details3");
+        addProduct("Company1","Details4");
+
+        addProduct("Company2","Details1");
+        addProduct("Company2","Details2");
+        addProduct("Company2","Details3");
+        addProduct("Company2","Details4");
+
+        addProduct("Company3","Details1");
+        addProduct("Company3","Details2");
+        addProduct("Company3","Details3");
+        addProduct("Company3","Details4");
+
+        addProduct("Company4","Details1");
+        addProduct("Company4","Details2");
+        addProduct("Company4","Details3");
+        addProduct("Company4","Details4");
+
+        addProduct("Company5","Details1");
+        addProduct("Company5","Details2");
+        addProduct("Company5","Details3");
+        addProduct("Company5","Details4");
+
+        addProduct("Company6","Details1");
+        addProduct("Company6","Details2");
+        addProduct("Company6","Details3");
+        addProduct("Company6","Details4");
+
+        addProduct("Company7","Details1");
+        addProduct("Company7","Details2");
+        addProduct("Company7","Details3");
+        addProduct("Company7","Details4");
+
+        addProduct("Company8","Details1");
+        addProduct("Company8","Details2");
+        addProduct("Company8","Details3");
+        addProduct("Company8","Details4");
+
+        addProduct("Company9","Details1");
+        addProduct("Company9","Details2");
+        addProduct("Company9","Details3");
+        addProduct("Company9","Details4");
+
+        addProduct("Company10","Details1");
+        addProduct("Company10","Details2");
+        addProduct("Company10","Details3");
+        addProduct("Company10","Details4");
+
+
+    }
+
+
+
+    //here we maintain our products in various departments
+    private int addProduct(String department, String product){
+
+        int groupPosition = 0;
+
+        //check the hash map if the group already exists
+        GroupInfo headerInfo = subjects.get(department);
+        //add the group if doesn't exists
+        if(headerInfo == null){
+            headerInfo = new GroupInfo();
+            headerInfo.setName(department);
+            subjects.put(department, headerInfo);
+            deptList.add(headerInfo);
+        }
+
+        //get the children for the group
+        ArrayList<ChildInfo> productList = headerInfo.getProductList();
+        //size of the children list
+        int listSize = productList.size();
+        //add to the counter
+        listSize++;
+
+        //create a new child and add that to the group
+        ChildInfo detailInfo = new ChildInfo();
+        detailInfo.setSequence(String.valueOf(listSize));
+        detailInfo.setName(product);
+        productList.add(detailInfo);
+        headerInfo.setProductList(productList);
+
+        //find the group position inside the list
+        groupPosition = deptList.indexOf(headerInfo);
+        return groupPosition;
+    }
+
 }
